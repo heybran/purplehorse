25189e1a7f1c31df0b7cf88a370b44806040757f
Brandon Zhang <gwonzhang@gmail.com>
2024-05-27 08:28:35 +0800

    convert to classic theme

--- patterns/hidden-search.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Title: Search
- * Slug: purplehorse/hidden-search
- * Inserter: no
- */
-?>
-
-<!-- wp:search {"label":"<?php echo esc_attr_x( 'Search', 'search form label', 'purplehorse' ); ?>","showLabel":false,"buttonText":"<?php echo esc_attr_x( 'Search', 'search button text', 'purplehorse' ); ?>","fontSize":"medium"} /-->
